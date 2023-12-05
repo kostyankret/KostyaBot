eec893a269521043b02193b04e4d17bc3bf1a3f3
kostyankret <139867315+mondlicht0@users.noreply.github.com>
2023-12-06 01:36:13 +0600

    fix

--- KostBot/video/management/commands/run_telegram_bot.py
+++ KostBot/video/management/commands/run_telegram_bot.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from shop.models import Video
+from video.models import Video
 import telebot
 
 bot = telebot.TeleBot("6987220975:AAHkjSUEP-AFtC8WC56-wfrSqbAN7OrOy4I")
